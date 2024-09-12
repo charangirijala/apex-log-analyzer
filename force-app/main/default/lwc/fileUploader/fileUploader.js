5fb7c200dfa592a54b430dc2367f16c957ce6373
Akashreddy19 <girijalac@gmail.com>
2024-09-12 22:50:09 +0530

    connect b/w Apex Controller and LWC done

--- force-app/main/default/lwc/fileUploader/fileUploader.js
+++ force-app/main/default/lwc/fileUploader/fileUploader.js
@@ -1,9 +1,14 @@
 import { LightningElement } from "lwc";
+import callApexFromClient from "@salesforce/apex/ClientInputHandler.callApexFromClient";
 export default class FileUploader extends LightningElement {
   fileUploaded = false;
   textAreaFilled = false;
+  textAreaData;
   fileNameLabel;
-  fileData;
+  fileData = {
+    fileName: "",
+    base64: ""
+  };
   get acceptedFormats() {
     return [".log", ".txt"];
   }
@@ -14,18 +19,16 @@ export default class FileUploader extends LightningElement {
   }
   textAreaChangeHandler(event) {
     // console.log("Data from inputAsText: " + JSON.stringify(event.detail));
-    const textAreaData = event.detail;
-    this.textAreaFilled = textAreaData.isValidData ? true : false;
+    this.textAreaData = event.detail;
+    this.textAreaFilled = this.textAreaData.isValidData ? true : false;
   }
   handleFileUpload(event) {
     const file = event.target.files[0];
     var reader = new FileReader();
     reader.onload = () => {
       var base64 = reader.result.split(",")[1];
-      this.fileData = {
-        fileName: file.name,
-        base64: base64
-      };
+      this.fileData.fileName = file.name;
+      this.fileData.base64 = base64;
       // console.log("FileData: " + JSON.stringify(this.fileData));
       this.fileNameLabel = "Uploaded File: " + this.fileData.fileName;
       this.fileUploaded = true;
@@ -34,18 +37,27 @@ export default class FileUploader extends LightningElement {
   }
   handleSubmit() {
     //prepare data here and call apex class
-    // console.log(
-    //   "FileUploaded: ",
-    //   this.fileUploaded,
-    //   " TextAreaFilled: ",
-    //   this.textAreaFilled
-    // );
-
+    const requestBody = {
+      type: "",
+      debugLogData: ""
+    };
     if (this.fileUploaded) {
       //process fileData
+      requestBody.type = "base64";
+      requestBody.debugLogData = this.fileData.base64;
     } else if (this.textAreaFilled) {
       //process textArea Data
+      requestBody.type = "text";
+      requestBody.debugLogData = this.textAreaData.logData;
     }
+    // console.log("RequestBody sent to client: ", JSON.stringify(requestBody));
+    callApexFromClient({ requestData: requestBody })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   get displayButton() {
