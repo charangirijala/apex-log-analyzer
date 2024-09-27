41a2a2207028e1ecdef4dc97dec6260fdbd3bbfc
Akashreddy19 <girijalac@gmail.com>
2024-09-27 21:21:41 +0530

    profilingInfo configured

--- force-app/main/default/lwc/LogRenderingComponents/lwc/profilingInfo/profilingInfo.js
+++ force-app/main/default/lwc/LogRenderingComponents/lwc/profilingInfo/profilingInfo.js
@@ -36,28 +36,156 @@ export default class ProfilingInfo extends LightningElement {
       //     profile.styling += this.stylingConfig(profile.value);
       //   }
       // });
+    } else {
+      console.log(
+        "Value of profiling info: ",
+        JSON.stringify(this.profilingInfo)
+      );
+      if (
+        this.profilingInfo.data.apiVersion !== null &&
+        this.profilingInfo.data.apiVersion !== undefined
+      ) {
+        this.apiVersion = "API : " + this.profilingInfo.data.apiVersion;
+      }
     }
   }
-  profilingStd = [
-    { key: "APEX_PROFILING", value: "-", styling: "slds-badge" },
-    { key: "APEX_CODE", value: "-", styling: "slds-badge" },
-    { key: "CALLOUT", value: "-", styling: "slds-badge" },
-    { key: "DB", value: "-", styling: "slds-badge" },
-    { key: "NBA", value: "-", styling: "slds-badge" },
-    { key: "SYSTEM", value: "-", styling: "slds-badge" },
-    { key: "VALIDATION", value: "-", styling: "slds-badge" },
-    { key: "WAVE", value: "-", styling: "slds-badge" },
-    { key: "VISUALFORCE", value: "-", styling: "slds-badge" },
-    { key: "WORKFLOW", value: "-", styling: "slds-badge" }
-  ];
+
+  get prof_values() {
+    const res = [];
+    const apexProfiling =
+      this.profilingInfo.data.APEX_PROFILING !== null &&
+      this.profilingInfo.data.APEX_PROFILING !== undefined
+        ? {
+            profile:
+              "APEX_PROFILING : " + this.profilingInfo.data.APEX_PROFILING,
+            theme: this.stylingConfig(this.profilingInfo.data.APEX_PROFILING)
+          }
+        : {
+            profile: "APEX_PROFILING : -",
+            theme: ""
+          };
+    res.push(apexProfiling);
+    const apexCode =
+      this.profilingInfo.data.APEX_CODE !== null &&
+      this.profilingInfo.data.APEX_CODE !== undefined
+        ? {
+            profile: "APEX_CODE : " + this.profilingInfo.data.APEX_CODE,
+            theme: this.stylingConfig(this.profilingInfo.data.APEX_CODE)
+          }
+        : {
+            profile: "APEX_CODE : -",
+            theme: ""
+          };
+    res.push(apexCode);
+    const callout =
+      this.profilingInfo.data.CALLOUT !== null &&
+      this.profilingInfo.data.CALLOUT !== undefined
+        ? {
+            profile: "CALLOUT : " + this.profilingInfo.data.CALLOUT,
+            theme: this.stylingConfig(this.profilingInfo.data.CALLOUT)
+          }
+        : {
+            profile: "CALLOUT : -",
+            theme: ""
+          };
+    res.push(callout);
+    const db =
+      this.profilingInfo.data.DB !== null &&
+      this.profilingInfo.data.DB !== undefined
+        ? {
+            profile: "DB : " + this.profilingInfo.data.DB,
+            theme: this.stylingConfig(this.profilingInfo.data.DB)
+          }
+        : {
+            profile: "DB : -",
+            theme: ""
+          };
+    res.push(db);
+    const nba =
+      this.profilingInfo.data.NBA !== null &&
+      this.profilingInfo.data.NBA !== undefined
+        ? {
+            profile: "NBA : " + this.profilingInfo.data.NBA,
+            theme: this.stylingConfig(this.profilingInfo.data.NBA)
+          }
+        : {
+            profile: "NBA : -",
+            theme: ""
+          };
+    res.push(nba);
+    const system =
+      this.profilingInfo.data.SYSTEM !== null &&
+      this.profilingInfo.data.SYSTEM !== undefined
+        ? {
+            profile: "SYSTEM : " + this.profilingInfo.data.SYSTEM,
+            theme: this.stylingConfig(this.profilingInfo.data.SYSTEM)
+          }
+        : {
+            profile: "SYSTEM : -",
+            theme: ""
+          };
+    res.push(system);
+    const validation =
+      this.profilingInfo.data.VALIDATION !== null &&
+      this.profilingInfo.data.VALIDATION !== undefined
+        ? {
+            profile: "VALIDATION : " + this.profilingInfo.data.VALIDATION,
+            theme: this.stylingConfig(this.profilingInfo.data.VALIDATION)
+          }
+        : {
+            profile: "VALIDATION : -",
+            theme: ""
+          };
+    res.push(validation);
+    const wave =
+      this.profilingInfo.data.WAVE !== null &&
+      this.profilingInfo.data.WAVE !== undefined
+        ? {
+            profile: "WAVE : " + this.profilingInfo.data.WAVE,
+            theme: this.stylingConfig(this.profilingInfo.data.WAVE)
+          }
+        : {
+            profile: "WAVE : -",
+            theme: ""
+          };
+    res.push(wave);
+
+    const workflow =
+      this.profilingInfo.data.WORKFLOW !== null &&
+      this.profilingInfo.data.WORKFLOW !== undefined
+        ? {
+            profile: "WORKFLOW : " + this.profilingInfo.data.WORKFLOW,
+            theme: this.stylingConfig(this.profilingInfo.data.WORKFLOW)
+          }
+        : {
+            profile: "WORKFLOW : -",
+            theme: ""
+          };
+    res.push(workflow);
+
+    const visualforce =
+      this.profilingInfo.data.VISUALFORCE !== null &&
+      this.profilingInfo.data.VISUALFORCE !== undefined
+        ? {
+            profile: "VISUALFORCE : " + this.profilingInfo.data.VISUALFORCE,
+            theme: this.stylingConfig(this.profilingInfo.data.VISUALFORCE)
+          }
+        : {
+            profile: "VISUALFORCE : -",
+            theme: ""
+          };
+    res.push(visualforce);
+
+    return res;
+  }
+
   stylingConfig(value) {
     if (value === "NONE") return "";
-    else if (value === "ERROR") return " slds-theme_error";
-    else if (value === "INFO") return " slds-badge_inverse";
-    else if (value === "WARN" || value === "DEBUG")
-      return " slds-theme_warning";
+    else if (value === "ERROR") return "slds-theme_error";
+    else if (value === "INFO") return "slds-badge_inverse";
+    else if (value === "WARN" || value === "DEBUG") return "slds-theme_warning";
     else if (value === "FINER" || value === "FINE" || value === "FINEST")
-      return " slds-theme_success";
+      return "slds-theme_success";
     return "";
   }
 }
--- force-app/main/default/lwc/badge/badge.js
+++ force-app/main/default/lwc/badge/badge.js
@@ -4,10 +4,20 @@ export default class Badge extends LightningElement {
   @api value;
   @api type;
   @api marginSide;
+  @api marginTop;
+  @api marginBottom;
   get badgeClass() {
     let temp = "slds-badge badge ";
     if (this.marginSide) {
       temp += "slds-var-m-horizontal_" + this.marginSide;
+      temp += " ";
+    }
+    if (this.marginTop) {
+      temp += "slds-var-m-top_" + this.marginTop;
+      temp += " ";
+    }
+    if (this.marginBottom) {
+      temp += "slds-var-m-bottom_" + this.marginBottom;
       temp += " ";
     }
     if (this.type) {
