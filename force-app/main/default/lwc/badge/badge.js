c8d62b30d9dac6998ff858200a2907bf3a93c097
Akashreddy19 <girijalac@gmail.com>
2024-10-01 22:50:03 +0530

    changes to badge

--- force-app/main/default/lwc/badge/badge.js
+++ force-app/main/default/lwc/badge/badge.js
@@ -1,6 +1,7 @@
 import { LightningElement, api } from "lwc";
 
 export default class Badge extends LightningElement {
+  //Implement badging API properties
   @api value;
   @api type;
   @api marginSide;
